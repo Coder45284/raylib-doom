20b3a530da2586fd07b4ffb6cabe0d3364456738
coder45284 <126514127+Coder45284@users.noreply.github.com>
2023-11-07 13:35:44 -0800

    Removed old linux code

--- src/r_data.h
+++ src/r_data.h
@@ -27,10 +27,6 @@
 #include "r_defs.h"
 #include "r_state.h"
 
-#ifdef __GNUG__
-#pragma interface
-#endif
-
 // Retrieve column data for span blitting.
 byte* R_GetColumn ( d_int tex, d_int col );
 
